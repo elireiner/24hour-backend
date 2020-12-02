aa7423e2de56a4696b6cc33ca1b95be4841f7c7a
Eli Reiners <goelyukim@gmail.com>
2020-12-02 09:48:35 -0500

    Initial commit

---
+++ src/products/add-products.js
@@ -0,0 +1,18 @@
+import ProductsService from "./products-service"
+const ProductsService = require('./products-service')
+
+/**
+ * 1. Get an array of products that need to added
+ * 2. loop over each product and:
+ *      a. post it to BigCommerce
+ *      b. if successful, mark as complete
+ *      c. if error, enter error in database
+ */
+export async function addProducts(db) {
+   const productsToAdd = ProductsService.getNonAddedProducts()
+   
+  await productsToAdd.map((product) => {
+    ProductsService.changeToAdded(db, product.id)
+   })
+   console.log(ProductsService.getNonAddedProducts())
+}
