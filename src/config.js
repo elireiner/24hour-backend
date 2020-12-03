3c65768e76dbac9f2a1e4f65c63ee3fb5de3d186
Eli Reiners <goelyukim@gmail.com>
2020-12-02 21:41:29 -0500

    Add big commerce url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    BIG_COMMERCE: `https://api.bigcommerce.com/stores/${process.env.STORE_ID}/v3`,
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://example: dunder_mifflin@localhost/blogful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://example: dunder_mifflin@localhost/blogful-test',
 }
