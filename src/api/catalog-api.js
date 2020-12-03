a6ffe1101f24e293a824b00aaf0cd3375d8e4e03
Eli Reiners <goelyukim@gmail.com>
2020-12-03 11:54:10 -0500

    Initial commit

---
+++ src/api/catalog-api.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const config = require('../config')
+//import TokenService from './token-service'
+const fetch = require('node-fetch');
+
+const catalogApiService = {
+
+    postNewItem(itemData, next) {
+        const uri = `${config.BIG_COMMERCE}/catalog/products`;
+
+        const headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'x-auth-token': `${process.env.x-auth-token}`
+        };
+
+        return fetch(uri, {
+            method: 'POST',
+            body: itemData,
+            headers: headers
+        })
+            .then(res => {
+                console.log(res)
+                return res
+            })
+            .catch(next)
+    }
+}
+
+module.exports = catalogApiService
