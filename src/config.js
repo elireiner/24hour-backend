0aaefedec8ec33979b58630dcb493f6355c41088
Eli Reiners <goelyukim@gmail.com>
2020-12-15 18:56:06 -0500

    Add apigenius base url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     BIG_COMMERCE: `https://api.bigcommerce.com/stores/${process.env.STORE_ID}/v3`,
+    APIGENIUS: `https://api.apigenius.io/products`,
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://example: dunder_mifflin@localhost/blogful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://example: dunder_mifflin@localhost/blogful-test',
 }
