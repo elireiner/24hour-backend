40ce8204295d485ed539903898e682f06d89c2db
Eli Reiners <goelyukim@gmail.com>
2020-12-22 07:59:58 -0500

    Update addProducts' logic

--- src/upcs/add-products-by-upc.js
+++ src/upcs/add-products-by-upc.js
@@ -1,19 +1,31 @@
 const apiGeniusApiService = require('../apis/apigeniuse-api');
-//const hardData = require('./hard-data')
-const upcHelperMethods = require('./upc-helper-methods')
+const UpcsService = require('./upc-service');
 
 const AddByUpcService = {
-    addProducts(hardData) {
-        const extractedData = upcHelperMethods.extractData(hardData)
-        return extractedData
+    addProducts(upcsArray, db, next) {
 
-        /*const productsToAdd = ProductsService.getNonAddedProducts()
-     
-       apiGeniusApiService.getItemData(upc, next)
-        .then(res => {
-        console.log(res)
-        })
-        .catch(next)*/
+        return Promise.all(upcsArray.reduce((acc, el) => {
+            acc = acc.concat(apiGeniusApiService.getItemData(el, next))
+            return acc
+        }, []))
+            .then((response) => {
+    
+                let successful = response.filter(itm => itm.success)
+                let successfulUPCS = successful.map(itm => Number(itm.items.upc))
+                let unsuccessful = upcsArray.filter(upc => !successfulUPCS.includes(Number(upc)))
+                return Promise.all(successful.reduce((acc, el) => {
+                    acc = acc.concat(UpcsService.insertUpcAndData(db, Number(el.items.upc), el))
+                    return acc
+                }, []))
+                    .then(response => {
+                       let addedUpcs = response.map(item => {
+                            return item.upc
+                        })
+                       return {"Success!": {addedUpcs}, "unsuccessful": {unsuccessful}}
+                    })
+                    .catch(next)
+            })
+            .catch(next)
     }
 }
 
