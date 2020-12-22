f0a8b3de3500b07935fede5ff6b1a516e19a3959
Eli Reiners <goelyukim@gmail.com>
2020-12-22 07:57:32 -0500

    Update

--- src/products/products-routers.js
+++ src/products/products-routers.js
@@ -2,46 +2,22 @@ const path = require('path');
 const express = require('express')
 //const xss = require('xss')
 //const debug = require('debug')('express:view')
-const ProductsService = require('./products-service');
+const ProductsService = require('../services/products-service');
 const AddProductsService = require('./add-products')
 
 const productsRouter = express.Router()
 const jsonParser = express.json()
 
-const serialize = product => ({
-    id: product.id,
-    info: product.info,
-    added: product.added
-})
-
 productsRouter
-    .route('/')
-    .post(jsonParser, (req, res, next) => {
-        const { products } = req.body;
-
-        if (!products) {
-            return res.status(400).json({
-                error: { message: `Missing 'products' in request body` }
-            })
-        }
-        if (!Array.isArray(products)) {
-            return res.status(400).json({
-                error: { message: `'Products' should be an array` }
-            })
-        }
-
-        ProductsService.insertProducts(
-            req.app.get('db'),
-            products
-        )
-            .then(async product => {
-                await AddProductsService.addProducts();
-                res
-                    .status(201)
-                    .location(path.posix.join(req.originalUrl, `/${product.id}`))
-                    .json(serialize(product))
-            })
-            .catch(next)
-    })
+  .route('/')
+  .get(async (req, res, next) => {
+    try {
+      let ids = await AddProductsService.addProducts(req.app.get('db'), next);
+      console.log(ids)
+      return res.status(200).json({ "Items Added": ids })
+    } catch (error) {
+      return res.status(400).end();
+    }
+  })
 
 module.exports = productsRouter
