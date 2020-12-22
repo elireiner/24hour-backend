e46cd0ef4662e631c0001c0284e6b8541ec65582
Eli Reiners <goelyukim@gmail.com>
2020-12-22 07:56:03 -0500

    Update with two complete endpoints

--- src/apis/catalog-api.js
+++ src/apis/catalog-api.js
@@ -1,30 +1,46 @@
 require('dotenv').config();
-const config = require('../config')
-//import TokenService from './token-service'
+const config = require('../config');
 const fetch = require('node-fetch');
 
 const catalogApiService = {
     postNewItem(itemData, next) {
-        return 'hi'
-        /* const uri = `${config.BIG_COMMERCE}/catalog/products`;
- 
-         const headers = {
-             'content-type': 'application/json',
-             'accept': 'application/json',
-             'x-auth-token': `${process.env.x-auth-token}`
-         };
- 
-         return fetch(uri, {
-             method: 'POST',
-             body: itemData,
-             headers: headers
-         })
-             .then(res => {
-                 console.log(res)
-                 return res
-             })
-             .catch(next)*/
-    }
+        const uri = `${config.BIG_COMMERCE}/catalog/products`;
+
+        const headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'x-auth-token': `${process.env.BIGCOMMERCE_TOKEN}`
+        };
+
+        return fetch(uri, {
+            method: 'POST',
+            headers: headers,
+            body: itemData,
+        })
+            .then(res => {
+                return res.json()
+            })
+            .catch(next)
+    },
+    postNewImage(itemData, id, next) {
+        const uri = `${config.BIG_COMMERCE}/catalog/products/${id}/images`;
+
+        const headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'x-auth-token': `${process.env.BIGCOMMERCE_TOKEN}`
+        };
+
+        return fetch(uri, {
+            method: 'POST',
+            headers: headers,
+            body: itemData,
+        })
+            .then(res => {
+                return res.json()
+            })
+            .catch(next)
+    },
 }
 
 module.exports = catalogApiService
