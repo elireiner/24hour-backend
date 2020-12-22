44625fd82eba6bcfca22f456c68d9350166d7f7a
Eli Reiners <goelyukim@gmail.com>
2020-12-22 08:01:20 -0500

    Update

--- src/upcs/upcs-routers.js
+++ src/upcs/upcs-routers.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
-const apiGeniusApiService = require('../apis/apigeniuse-api')
-const AddByUpcService = require('./add-products-by-upc')
+const AddByUpcService = require('./add-products-by-upc');
 //const xss = require('xss')
 //const debug = require('debug')('express:view')
 //const UpcsService = require('./upcs-service');
@@ -20,16 +19,13 @@ upcsRouter
     .route('/hard-data')
     .post(jsonParser, (req, res, next) => {
 
-        Promise.all(req.body.data.reduce((acc, el) => {
-            acc = acc.concat(apiGeniusApiService.getItemData(el, next))
-            return acc
-        }, [])).then((results) => {
-            return res.status(201).json(results)
-        }).catch((err) => {
-            return res.status(400).end();
-        })
-
-        //  const resData = await AddByUpcService.addProducts(hardData)
+        AddByUpcService.addProducts(req.body.data, req.app.get('db'), next)
+            .then((results) => {
+                console.log(results)
+                return res.status(201).json(results)
+            }).catch((err) => {
+                return res.status(400).end();
+            })
     })
 
 upcsRouter
